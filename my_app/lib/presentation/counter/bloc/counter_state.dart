f17ddca79932bbf8d5acf3ed54b7eb6784efb331
NanoNish <nishkarsh1206@gmail.com>
2022-06-15 12:32:02 +0530

    rfac: Major refactor and directory organization

---
+++ my_app/lib/presentation/counter/bloc/counter_state.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
+  const CounterState({
+    required this.counter,
+  });
+
+  final int counter;
+  @override
+  List<Object?> get props => <Object?>[];
+}
